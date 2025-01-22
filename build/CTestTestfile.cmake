59b7d42f499218be41f541a6b40f8612aad640cc
Martins-Lucaas <lucaspmartins14@gmail.com>
2025-01-22 14:15:02 -0300

    ros2_changes

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lucas-pc/RoboticArm/src
-# Build directory: /home/lucas-pc/RoboticArm/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("robotic_arm")
--- build/robotic_arm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lucas-pc/RoboticArm/src/robotic_arm
-# Build directory: /home/lucas-pc/RoboticArm/build/robotic_arm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
